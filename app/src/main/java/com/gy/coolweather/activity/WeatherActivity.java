53c47e248c9c0b1fc1ffeea4a24b47003f91ac98
Yang <gaoyangml@126.com>
2018-06-26 22:55:27 +0800

    创建天气预报界面

---
+++ app/src/main/java/com/gy/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,20 @@
+package com.gy.coolweather.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gy.coolweather.R;
+
+/**
+ * Created by yanggao on 18/6/18.
+ */
+public class WeatherActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
